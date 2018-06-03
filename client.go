cde233825dbc70f0e0ff589835d9c8f86e8f4431
Sihao Fang <sihao@momenta.ai>
2018-06-03 23:13:29 +0800

    [feature] add client and command interface

--- client.go
+++ client.go
@@ -27,10 +27,10 @@ func createConn(conn *transport.TCPConn) *Client {
 	return &Client{conn: conn}
 }
 
-func (c *Client) CreateTask(topic string, trigger int64) {
+func (c *Client) CreateTask(topic, content string, trigger int64) {
 	var request protocol.Request
 	request.Command = []byte(topic)
-	request.Parameters = append(request.Parameters, []byte(strconv.FormatInt(trigger, 10)))
+	request.Parameters = append(request.Parameters, []byte(content), []byte(strconv.FormatInt(trigger, 10)))
 	request.Version = 1
 	data, err := proto.Marshal(&request)
 	if err != nil {
@@ -40,4 +40,22 @@ func (c *Client) CreateTask(topic string, trigger int64) {
 		panic(err)
 	}
 	c.prot.WriteMessage(c.conn, data)
+}
+
+func (c *Client) DoCommand(cmd Command) {
+	req := cmd.Request()
+	req.Version = 1
+	c.SendRequest(req)
+}
+
+func (c *Client) SendRequest(req *protocol.Request) error {
+	data, err := proto.Marshal(req)
+	if err != nil {
+		panic(err)
+	}
+	if err = c.prot.WriteUint16(c.conn, uint16(len(data))); err != nil {
+		panic(err)
+	}
+	c.prot.WriteMessage(c.conn, data)
+	return nil
 }
---
+++ version.go
@@ -0,0 +1,9 @@
+package lastrip
+
+type version struct {
+	Major int32
+	Minor int32
+	Build int32
+}
+
+var buildTime string
